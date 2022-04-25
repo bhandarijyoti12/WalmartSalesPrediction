6968281fedf647e4da011f6bf6785ade39c0a45f
bhandarijyoti12 <jyotibhandarii32@outlook.com>
2022-04-25 12:32:24 -0400

    2nd commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+import numpy as np
+import pickle
+from sklearn.preprocessing import OneHotEncoder
+import joblib
+import pandas as pd
+
+
+app = Flask(__name__)
+model = pickle.load(open('model4.pkl', 'rb'))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    temperature = request.form.get('temperature')
+    fuel_price = request.form.get('fuel_price')
+    Cpi = request.form.get('CPI')
+    Unemployment = request.form.get('Unemployment')
+    Day= request.form.get('Day')
+    Month=request.form.get('Month')
+    Holiday=request.form.get('Holiday')
+    store= int(request.form.get('store'))
+    print('store:', store)
+    S = np.zeros(44)
+    for i in range(1,45): 
+          if (i==store):
+            S[i-1]= 1 
+    print('S=', S)
+    arr1 = np.array([temperature, fuel_price, Cpi, Unemployment,Day, Month, Holiday ])
+    final_arr= np.concatenate((arr1,S))
+    final_arr = final_arr.astype(np.float64)
+    print(final_arr)
+    pred = model.predict([final_arr])
+    output=round(pred[0],2)
+    return render_template('index.html', data=output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
